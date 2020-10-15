826ebc5e352b26a976ae5f738bb35af4c2d5d214
Max Shafeev <shafeevms@gmail.com>
2020-10-15 23:35:23 +0300

    obj added

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const textarea = document.getElementById('textarea');
 const addTaskButton = document.getElementById('form-btn');
 const ul = document.querySelector('.tasks__list');
 let activeNum = document.querySelector('.active_num');
-const tasks = [];
+let perfomedNum = document.querySelector('.perfomed_num')
+let tasks = [];
+let perfomed = [];
 
 
 // так не работает... почему?
@@ -21,7 +23,7 @@ const tasks = [];
 
 addTaskButton.addEventListener('click', function(e) {
     e.preventDefault();
-    if(input.value && textarea.value) {
+    if (input.value && textarea.value) {
         input.classList.remove('alert');
         textarea.classList.remove('alert');
         tasks.push({
@@ -31,8 +33,7 @@ addTaskButton.addEventListener('click', function(e) {
         let li = document.createElement('li');
         li.className = 'task__item';
         li.innerHTML = `<article class="task__article">
-                            <input type="checkbox" class="checkbox" id="checkbox">
-                            <label class="label label-done" for="checkbox" aria-label="mark task"></label>
+                            <button class="label" id="checkbox"></button>
                             <button class="task__rollup" id="roll" aria-label="roll up the description"></button>
                             <button class="task__clear" id="delete" aria-label="delete the description"></button>
                             <h2 class="taks__title">${input.value}</h2>
@@ -50,14 +51,48 @@ addTaskButton.addEventListener('click', function(e) {
     
 });
 
-// этой кнопки не было при загрузке ДОМа - как ее найти?  говорят про какое-то делегирование))
+ul.addEventListener('click', function(event) {
+    let target = event.target;
+    if (target.id == 'delete') {
+        
+        target.parentElement.parentElement.remove();
+        tasks = tasks.filter(el => el.title != target.nextElementSibling.textContent 
+            && el.description != target.nextElementSibling.nextElementSibling.textContent);
+        activeNum.textContent = '(' + tasks.length + ')';
+    };
+    if (target.id == 'roll') {
+        console.log(target.nextElementSibling.nextElementSibling.nextElementSibling, target);
+        target.nextElementSibling.nextElementSibling.nextElementSibling.classList.toggle('hide');
+        target.classList.toggle('rollup')
+    };
+    
+    // при нажатии на левую кнопку задача уходит в perfomed 
 
-// const deleteButton = document.getElementById('delete');
+    if (target.id == 'checkbox') {
+        // target.nextElementSibling.classList.toggle('done-green');
+    //     perfomed.push(tasks.find(el => {
+    //         el.title == target.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent
+    //         && el.description == target.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent
+    //     }))
+    //     activeNum.textContent = '(' + tasks.length + ')';
+    //     perfomedNum.textContent = '(' + tasks.length + ')';
+    }
 
-// deleteButton.addEventListener('click', function(e) {
-//     e.preventDefault();
-//     target = e.target;
-//     console.log(target)
-// });
+        let tempTitle = target.nextElementSibling.nextElementSibling.nextElementSibling.textContent;
+        let tempDescr = target.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent 
+        console.log(tempTitle, tempDescr, tasks)
 
+        // вот все переменные совпадают а в цикле они почему-то не работают
+        tasks.forEach(function(el) {
+            console.log(el.title, tempTitle);
+            console.log(el.description, tempDescr)
+            if (el.title == tempTitle && el.description == tempDescr) {
+                console.log('yes');
+            }
+    })
+    // вернуть из массива tasks (удалить из него) и вложить в массив perfomed
+        let temp = tasks.find(el => {el.title == tempTitle && el.description == tempDescr})
+    
+});
 
+// не получается скручивать кнопочку "свернуть"
